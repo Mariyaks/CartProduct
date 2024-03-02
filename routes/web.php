c1427e3b90fff31bb6a910b0f86b7d728b1e49bb
Mariyaks <mariyakarekkattu@gmail.com>
2024-03-02 11:01:40 +0530

    Admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/', function () {return redirect('login');})->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->name('register');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
